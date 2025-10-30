ff4af578ded24becf882a4d24a9578d0c61d8baa
Adam Holt <me@adamholt.co.uk>
2025-10-30 16:46:17 +0100

    More detailed descriptions

--- pkg/github/actions.go
+++ pkg/github/actions.go
@@ -68,7 +68,10 @@ func actionFromString(s string) actionsActionType {
 
 func ActionsRead(getClient GetClientFn, t translations.TranslationHelperFunc) (tool mcp.Tool, handler server.ToolHandlerFunc) {
 	return mcp.NewTool("actions_read",
-			mcp.WithDescription(t("TOOL_ACTIONS_READ_DESCRIPTION", "Tools for reading GitHub Actions resources. Use this tool to get details about individual Actions Workflows, list and get individual Actions Workflow Runs, Jobs, and Artifacts.")),
+			mcp.WithDescription(t("TOOL_ACTIONS_READ_DESCRIPTION", `Tools for reading GitHub Actions resources.
+Use this tool to list workflows in a repository, or list workflow runs, jobs, and artifacts for a specific workflow or workflow run.
+Use this tool also to get details about individual workflows, workflow runs, jobs, and artifacts, by using their unique IDs.
+`)),
 			mcp.WithToolAnnotation(mcp.ToolAnnotation{
 				Title:        t("TOOL_ACTIONS_READ_USER_TITLE", "Get details of GitHub Actions resources (workflows, workflow runs, jobs, and artifacts)"),
 				ReadOnlyHint: ToBoolPtr(true),
