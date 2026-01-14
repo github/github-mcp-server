b54e64ea83a19881ba61146f3e4d6fd51c3b122b
tonytrg <tonytrg@github.com>
2026-01-14 21:43:45 +0100

    adding test var

--- internal/ghmcp/server_test.go
+++ internal/ghmcp/server_test.go
@@ -23,6 +23,7 @@ func TestNewMCPServer_CreatesSuccessfully(t *testing.T) {
 		Translator:        translations.NullTranslationHelper,
 		ContentWindowSize: 5000,
 		LockdownMode:      false,
+		Experimental:      false,
 	}
 
 	// Create the server
