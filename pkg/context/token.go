3ce3c3bd8090c45e95e901e57c1b5ecb6fbf6802
Adam Holt <me@adamholt.co.uk>
2026-02-16 12:34:04 +0100

    Use anonyamous structs as context keys to avoid collisions and ensure type safety.

--- pkg/context/token.go
+++ pkg/context/token.go
@@ -6,10 +6,7 @@ import (
 	"github.com/github/github-mcp-server/pkg/utils"
 )
 
-// tokenCtxKey is a context key for authentication token information
-type tokenCtx string
-
-var tokenCtxKey tokenCtx = "tokenctx"
+type tokenCtxKey struct{}
 
 type TokenInfo struct {
 	Token     string
@@ -18,29 +15,27 @@ type TokenInfo struct {
 
 // WithTokenInfo adds TokenInfo to the context
 func WithTokenInfo(ctx context.Context, tokenInfo *TokenInfo) context.Context {
-	return context.WithValue(ctx, tokenCtxKey, tokenInfo)
+	return context.WithValue(ctx, tokenCtxKey{}, tokenInfo)
 }
 
 // GetTokenInfo retrieves the authentication token from the context
 func GetTokenInfo(ctx context.Context) (*TokenInfo, bool) {
-	if tokenInfo, ok := ctx.Value(tokenCtxKey).(*TokenInfo); ok {
+	if tokenInfo, ok := ctx.Value(tokenCtxKey{}).(*TokenInfo); ok {
 		return tokenInfo, true
 	}
 	return nil, false
 }
 
-type TokenScopesKey tokenCtx
-
-var tokenScopesKey TokenScopesKey = "tokenscopesctx"
+type tokenScopesKey struct{}
 
 // WithTokenScopes adds token scopes to the context
 func WithTokenScopes(ctx context.Context, scopes []string) context.Context {
-	return context.WithValue(ctx, tokenScopesKey, scopes)
+	return context.WithValue(ctx, tokenScopesKey{}, scopes)
 }
 
 // GetTokenScopes retrieves token scopes from the context
 func GetTokenScopes(ctx context.Context) ([]string, bool) {
-	if scopes, ok := ctx.Value(tokenScopesKey).([]string); ok {
+	if scopes, ok := ctx.Value(tokenScopesKey{}).([]string); ok {
 		return scopes, true
 	}
 	return nil, false
