f55627e53e2d73f0439e99afc788e10961fb29ab
Omid Mogasemi <omid@anthropic.com>
2026-03-20 19:02:46 +0000

    fix: restore thread id in get_review_comments response

--- pkg/github/minimal_types.go
+++ pkg/github/minimal_types.go
@@ -1,6 +1,7 @@
 package github
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/google/go-github/v82/github"
@@ -717,6 +718,7 @@ type MinimalReviewComment struct {
 
 // MinimalReviewThread is the trimmed output type for PR review thread objects.
 type MinimalReviewThread struct {
+	ID          string                 `json:"id"`
 	IsResolved  bool                   `json:"is_resolved"`
 	IsOutdated  bool                   `json:"is_outdated"`
 	IsCollapsed bool                   `json:"is_collapsed"`
@@ -853,6 +855,7 @@ func convertToMinimalReviewThread(thread reviewThreadNode) MinimalReviewThread {
 	}
 
 	return MinimalReviewThread{
+		ID:          fmt.Sprintf("%v", thread.ID),
 		IsResolved:  bool(thread.IsResolved),
 		IsOutdated:  bool(thread.IsOutdated),
 		IsCollapsed: bool(thread.IsCollapsed),
